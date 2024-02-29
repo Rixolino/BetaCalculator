2780954eed6b57d5cb8eee67c19bccebb3f442f8
Simone Risola #19 <71252421+Rixolino@users.noreply.github.com>
2024-02-29 20:10:03 +0100

    Add files via upload

--- My Project/Settings.Designer.vb
+++ My Project/Settings.Designer.vb
@@ -15,7 +15,7 @@ Option Explicit On
 Namespace My
     
     <Global.System.Runtime.CompilerServices.CompilerGeneratedAttribute(),  _
-     Global.System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.3.0.0"),  _
+     Global.System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.9.0.0"),  _
      Global.System.ComponentModel.EditorBrowsableAttribute(Global.System.ComponentModel.EditorBrowsableState.Advanced)>  _
     Partial Friend NotInheritable Class MySettings
         Inherits Global.System.Configuration.ApplicationSettingsBase
@@ -64,9 +64,9 @@ Namespace My
     Friend Module MySettingsProperty
         
         <Global.System.ComponentModel.Design.HelpKeywordAttribute("My.Settings")>  _
-        Friend ReadOnly Property Settings() As Global.BetaCloud_Calculator.My.MySettings
+        Friend ReadOnly Property Settings() As Global.BetaCalculator.My.MySettings
             Get
-                Return Global.BetaCloud_Calculator.My.MySettings.Default
+                Return Global.BetaCalculator.My.MySettings.Default
             End Get
         End Property
     End Module
